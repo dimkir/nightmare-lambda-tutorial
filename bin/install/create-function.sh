392958b1624cbab3d9aa062e87b12d49979ed5b2
Dimkir Mars <dimkir+mars@gmail.com>
2017-03-18 01:11:54 +0000

    adds missing 60 second timeout for function creation

--- bin/install/create-function.sh
+++ bin/install/create-function.sh
@@ -18,16 +18,16 @@ echo "Number of parameters is $#"
 if [ $# -ne 3 ]
 then
     CMD="./$(basename $0)"
-    echo 
+    echo
     echo "  Usage:"
     echo
     echo "   $CMD    <function-name-or-alias> <role-arn> <function-package.zip>"
     echo
     echo "  Example: "
-    echo 
+    echo
     echo "  $CMD    $PROJECT_NAME    arn:xxxx     var/dist/$PROJECT_NAME.zip"
-    echo 
-    echo 
+    echo
+    echo
 
     exit 1
 fi
@@ -43,8 +43,8 @@ echo "Function package:\t   $package"
 aws lambda create-function \
     --function $function \
     --memory-size 1024 \
+    --timeout 60 \
     --runtime nodejs4.3 \
     --handler index.handler \
     --role $role_arn \
     --zip-file fileb://./$package
-
